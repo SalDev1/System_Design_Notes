ea8da0d1c826f7cbe79f66c913870f4da6aa53ce
Salman Uddin <salmanuddin9820@gmail.com>
2024-06-26 19:11:05 +0530

    Understood LLD of TicTacToe

---
+++ LLD/DesignTicTacToe/Board.java
@@ -0,0 +1,52 @@
+import com.sun.tools.javac.util.Pair;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Board {
+
+    public int size;
+    public PlayingPiece[][] board;
+
+    public Board(int size) {
+        this.size = size;
+        board = new PlayingPiece[size][size];
+    }
+
+    public boolean addPiece(int row, int col, PlayingPiece playingPiece) {
+        if(board[row][col] != null) {
+            return false;
+        }
+        board[row][col] = playingPiece;
+        return true;
+    }
+
+    public List<List<Integer>> getFreeCells() {
+        List<List<Integer>> freeCells = new ArrayList<>();
+
+        for(int i = 0; i < size; i++) {
+            for(int j = 0 ; j < size; j++) {
+                if(board[i][j] == null) {
+                    List<Integer> rowColumn = new ArrayList<>();
+                    rowColumn.add(i);
+                    rowColumn.add(j);
+                    freeCells.add(rowColumn);
+                }
+            }
+        }
+        return freeCells;
+    }
+
+    public void printBoard() {
+        for(int i = 0 ; i < size ; i++) {
+            for(int j = 0 ; j < size ; j++) {
+                if (board[i][j] != null) {
+                    System.out.println(board[i][j].pieceType.name() + "   |");
+                } else {
+                    System.out.println(" | ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ LLD/DesignTicTacToe/PlayingPiece.java
@@ -0,0 +1,15 @@
+public class PlayingPiece {
+    PieceType pieceType;
+
+    public PlayingPiece(PieceType pieceType) {
+        this.pieceType = pieceType;
+    }
+    public PieceType getPieceType() {
+        return pieceType;
+    }
+
+    public void setPieceType(PieceType pieceType) {
+        this.pieceType = pieceType;
+    }
+
+}
